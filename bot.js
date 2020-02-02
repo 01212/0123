818375c81fbd115985ea468ebb9885f6c7877bf6
01212 <60580639+01212@users.noreply.github.com>
2020-02-02 22:24:07 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get(672786845180100639ا")
+setInterval(function() {
+channel.send(`672786845180100639`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
